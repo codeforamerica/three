3de70abea3523ab09501315bd04218e37474de15
Zach Williams <hey@zachwill.com>
2012-03-15 14:11:22 -0700

    Add additional cities

--- three/cities.py
+++ three/cities.py
@@ -7,8 +7,10 @@ class CityNotFound(Exception):
     pass
 
 
-def find_info(name):
+def find_info(name=None):
     """Find the needed city server information."""
+    if not name:
+        return servers.keys()
     name = name.lower()
     if name in servers:
         info = servers[name]
@@ -21,11 +23,23 @@ servers = {
     'baltimore': {
         'endpoint': 'http://311.baltimorecity.gov/open311/v2/'
     },
+    'bloomington': {
+        'endpoint': 'https://bloomington.in.gov/crm/open311/v2/'
+    },
     'boston': {
         'endpoint': 'https://mayors24.cityofboston.gov/open311/v2/'
     },
+    'brookline': {
+        'endpoint': 'http://spot.brooklinema.gov/open311/v2/'
+    },
     'grand rapids': {
         'endpoint': 'http://grcity.spotreporters.com/open311/v2/'
+    },
+    'macon': {
+        'endpoint': 'http://seeclickfix.com/macon/open311/'
+    },
+    'new haven': {
+        'endpoint': 'http://seeclickfix.com/new-haven/open311/'
     },
     'san francisco': {
         'endpoint': 'https://open311.sfgov.org/V2/',
@@ -36,5 +50,9 @@ servers = {
         'endpoint': 'https://open311.sfgov.org/V2/',
         'format': 'xml',
         'jurisdiction': 'sfgov.org'
+    },
+    'toronto': {
+        'endpoint': 'https://secure.toronto.ca/webwizard/ws/',
+        'jurisdiction': 'toronto.ca'
     },
 }

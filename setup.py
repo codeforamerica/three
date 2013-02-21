08707e7cd13a0a95c7a9b7120e89a6b0a032ab3a
Zach Williams <hey@zachwill.com>
2013-02-20 22:20:16 -0800

    Update version to include @fletom's changes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name="three",
-    version="0.7",
+    version="0.7.1",
     url="http://github.com/codeforamerica/three",
     author="Zach Williams",
     author_email="hey@zachwill.com",
